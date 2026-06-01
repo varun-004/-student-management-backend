1dc5ac8d94aaa094606f6f91dd2aa870928bc043
Varun G A <gavarunvarun@gmail.com>
2026-06-01 23:30:25 +0530

    frontedn cors error fixed

--- src/main/java/com/example/student_management_system/config/CorsConfig.java
+++ src/main/java/com/example/student_management_system/config/CorsConfig.java
@@ -2,32 +2,30 @@ package com.example.student_management_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public WebMvcConfigurer corsConfigurer() {
 
-        CorsConfiguration config =
-                new CorsConfiguration();
+        return new WebMvcConfigurer() {
 
-        config.addAllowedOrigin("http://localhost:5173");
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
 
-        config.addAllowedHeader("*");
-
-        config.addAllowedMethod("*");
-
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:5174",
+                                "http://localhost:5175"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
